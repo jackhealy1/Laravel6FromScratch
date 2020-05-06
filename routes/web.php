33a76b27256265f968c169dccd36e11bc0f1fde9
Jack Healy <Jack@users-mbp-4.home>
2020-05-06 14:50:36 +0100

    Integrate SimpleWork site template and create about page
    Use templated.co to find SimpleWork template, then use blade templating to create 2 different pages with shared layout

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/contact', function () {
     return view('contact');
 });
