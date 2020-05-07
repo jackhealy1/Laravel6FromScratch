247ebc7a87d4299ac62fd69e0e74a94b0bb2743b
Jack Healy <Jack@users-mbp-4.home>
2020-05-07 11:40:26 +0100

    Set active menu link and compile css assests
    Update layout to include active highlighting for menu links and then use the mix wrapper around webpack to assist with css and sass bundling and compilation

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,7 +2,6 @@
 
 @section ('content')
 
-
 <div id="wrapper">
 	<div id="page" class="container">
 		<div id="content">
@@ -52,7 +51,5 @@
 		</div>
 	</div>
 </div>
-<div id="copyright" class="container">
-	<p>&copy; Untitled. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a> | Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
-</div>
+
 @endsection
